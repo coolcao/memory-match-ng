32660a9af33db23d290bd76af5ff9847bbcc7a70
coolcao <me@coolcao.com>
2025-02-10 14:36:00 +0800

    修改路由为根

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'memory-match', pathMatch: 'full' },
-  { path: 'memory-match', loadChildren: () => import('./memory-match/memory-match.module').then(m => m.MemoryMatchModule) },
+  { path: '', loadChildren: () => import('./memory-match/memory-match.module').then(m => m.MemoryMatchModule) },
 ];
 
 @NgModule({
